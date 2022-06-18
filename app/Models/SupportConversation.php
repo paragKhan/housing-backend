c19c884cccdecccb40ebcc6307155ce2c7a1fbad
paragKhan <prgkhan@gmail.com>
2022-06-18 15:10:40 +0800

    replied by last admin

--- app/Models/SupportConversation.php
+++ app/Models/SupportConversation.php
@@ -10,14 +10,19 @@ class SupportConversation extends Model
     use HasFactory;
 
     protected $guarded = [];
+    protected $appends = ['last_admin'];
+
+    public function getLastAdminAttribute(){
+        return $this->support_messages()->where('senderable_type', '!=', User::class)->first()->senderable->name;
+    }
 
     public function user(){
         return $this->belongsTo(User::class);
     }
-    public function staff(){
+    public function staff()
+    {
         return $this->belongsTo(Staff::class);
     }
-
     public function support_messages(){
         return $this->hasMany(SupportMessage::class);
     }
