c9dfae9d7b7424670e590f55d866ab123e9c0e62
paragKhan <prgkhan@gmail.com>
2022-11-05 01:20:40 +0600

    Form request for RTO

--- app/Http/Requests/UpdateRTORequest.php
+++ app/Http/Requests/UpdateRTORequest.php
@@ -14,7 +14,7 @@ class UpdateRTORequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     public function prepareForValidation()
